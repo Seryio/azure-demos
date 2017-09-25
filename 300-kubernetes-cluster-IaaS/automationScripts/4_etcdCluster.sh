30dc973c561ce5288b52ffc3e24e597e1b37d9be
seryio <sergio.gonzalez.jimenez@gmail.com>
2017-09-25 11:46:30 +0200

    back to HTTPS

--- 300-kubernetes-cluster-IaaS/automationScripts/4_etcdCluster.sh
+++ 300-kubernetes-cluster-IaaS/automationScripts/4_etcdCluster.sh
@@ -42,10 +42,18 @@ Documentation=https://github.com/coreos
 [Service]
 ExecStart=/usr/local/bin/etcd \\
   --name ${MASTER_NAME} \\
-  --initial-advertise-peer-urls http://${MASTER_INTERNAL_IP}:2380 \\
-  --listen-peer-urls http://${MASTER_INTERNAL_IP}:2380 \\
-  --listen-client-urls http://${MASTER_INTERNAL_IP}:2379,http://127.0.0.1:2379 \\
-  --advertise-client-urls http://${MASTER_INTERNAL_IP}:2379 \\
+  --cert-file=/etc/etcd/kubernetes.pem \\
+  --key-file=/etc/etcd/kubernetes-key.pem \\
+  --peer-cert-file=/etc/etcd/kubernetes.pem \\
+  --peer-key-file=/etc/etcd/kubernetes-key.pem \\
+  --trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --initial-advertise-peer-urls https://${MASTER_INTERNAL_IP}:2380 \\
+  --listen-peer-urls https://${MASTER_INTERNAL_IP}:2380 \\
+  --listen-client-urls https://${MASTER_INTERNAL_IP}:2379,http://127.0.0.1:2379 \\
+  --advertise-client-urls https://${MASTER_INTERNAL_IP}:2379 \\
   --initial-cluster-token etcd-cluster-0 \\
   --initial-cluster ${CLUSTER_CONNECTION_STRING} \\
   --initial-cluster-state new \\
